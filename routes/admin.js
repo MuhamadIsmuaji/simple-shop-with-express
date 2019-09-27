a3a3fb287b616612c48421c4465dc855e3fe27d6
MuhamadIsmuaji <muhamadismuaji@gmail.com>
2019-09-27 18:32:27 +0700

    sharing data accross users

--- routes/admin.js
+++ routes/admin.js
@@ -3,14 +3,19 @@ const router = express.Router();
 const path = require('path');
 const rootDir = require('../utils/path');
 
+const products = [];
+
 router.get('/add-product', (req, res, next) => {
     // res.send('<form action="/admin/add-product" method="POST"><input type="text" name="title" /><button type="submit">Submit</button></form>')
     res.sendFile(path.join(rootDir, 'views', 'add-product.html'));
 });
 
 router.post('/add-product', (req, res, next) => {
-    console.log(req.body);
+    // console.log(req.body);
+    products.push({ title: req.body.title });
     res.redirect('/');
 });
 
-module.exports = router;
+// module.exports = router;
+exports.routes = router;
+exports.products = products;
--- routes/shop.js
+++ routes/shop.js
@@ -3,8 +3,11 @@ const router = express.Router();
 const path = require('path');
 const rootDir = require('../utils/path');
 
+const adminData = require('../routes/admin');
+
 router.get('/', (req, res, next) => {
     // res.send('<h1>Hello from express</h1>');
+    console.log(adminData.products);
     res.sendFile(path.join(rootDir, 'views', 'shop.html'));
 });
 
