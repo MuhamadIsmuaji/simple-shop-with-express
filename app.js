9bd1dc2c9ab9d10186d067e97f002d0ffcff1e1a
MuhamadIsmuaji <muhamadismuaji@gmail.com>
2019-10-01 15:36:58 +0700

    converting html to pug

--- app.js
+++ app.js
@@ -18,7 +18,10 @@ app.use('/admin', adminData.routes);
 app.use(shopRouter);
 app.use((req, res, next) => {
     // res.status(404).send('<h1>Page not found</h1>');
-    res.status(404).sendFile(path.join(__dirname, 'views', '404.html'));
+    // res.status(404).sendFile(path.join(__dirname, 'views', '404.html'));
+
+    // use templating engine
+    res.render('404');
 });
 
 app.listen(3000);
